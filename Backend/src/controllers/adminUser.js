e139842cdc286b611f0d0704af3ac5b8a03acd4c
Avnish Negi <101538378+28avnish@users.noreply.github.com>
2025-09-01 18:49:52 +0530

    Add files via upload

--- Backend/src/controllers/adminUser.js
+++ Backend/src/controllers/adminUser.js
@@ -3,7 +3,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import ErrorResponse from "../utils/errorResponse.js";
 import jwt from "jsonwebtoken";
 
-
 export const newAdminUser = asyncHandler(async (req, res, next) => {
   const { name, email, password, roleType, modules } = req.body;
 
@@ -18,7 +17,7 @@ export const newAdminUser = asyncHandler(async (req, res, next) => {
     name,
     email,
     password,
-    roleType,   // "admin" | "superadmin"
+    roleType, // "admin" | "superadmin"
     modules: roleType === "superadmin" ? [] : modules, // enforce rule
   });
 
@@ -34,12 +33,17 @@ export const adminLogin = async (req, res, next) => {
 
   const user = await AdminUser.findOne({ email });
 
- if (user.status === "suspended") {
-  return next(new ErrorResponse("Your account has been suspended. Please contact the Super Admin.", 403));
- }
-
   if (!user || !(await user.matchPassword(password))) {
     return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  if (user.status === "suspended") {
+    return next(
+      new ErrorResponse(
+        "Your account has been suspended. Please contact the Super Admin.",
+        403
+      )
+    );
   }
 
   // Generate token
@@ -62,7 +66,6 @@ export const adminLogin = async (req, res, next) => {
 };
 
 export const adminLogout = async (req, res, next) => {
-  
   res.clearCookie("ADMIN_TOKEN_KAROFLIGHT", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
@@ -77,8 +80,7 @@ export const adminLogout = async (req, res, next) => {
 
 // UPDATE ADMIN USER (only superadmin)
 export const updateAdminUser = asyncHandler(async (req, res, next) => {
-  const { id } = req.params; // admin user id to update
-  const { name, email, roleType, modules , password , status  } = req.body;
+  const { name, email, roleType, modules, password, status, id } = req.body;
 
   // ✅ find user to update
   const user = await AdminUser.findById(id);
@@ -98,10 +100,10 @@ export const updateAdminUser = asyncHandler(async (req, res, next) => {
   if (roleType === "superadmin") {
     user.modules = []; // force reset
   }
-    if (status) {
+  if (status) {
     user.status = status;
   }
-   if (password) {
+  if (password) {
     user.password = password; // will be hashed by pre-save hook
   }
 
@@ -137,4 +139,23 @@ export const deleteAdminUser = asyncHandler(async (req, res, next) => {
   });
 });
 
+export const getAllAdminsExceptSuperAdmin = asyncHandler(async (req, res) => {
+  try {
+    const currentUserId = req.user._id;
 
+    // Fetch all users except current user
+    const admins = await AdminUser.find({
+      _id: { $ne: currentUserId }, // exclude current user
+    })
+      .select("-password")
+      .sort({ roleType: -1 }); // exclude password field
+
+    res.status(200).json({
+      success: true,
+      data: admins,
+    });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error fetching admins");
+  }
+});
--- Backend/src/routes/adminUser.js
+++ Backend/src/routes/adminUser.js
@@ -1,14 +1,26 @@
 import express from "express";
-import { adminLogin, adminLogout, deleteAdminUser, newAdminUser, updateAdminUser } from "../controllers/adminUser.js";
+import {
+  adminLogin,
+  adminLogout,
+  deleteAdminUser,
+  getAllAdminsExceptSuperAdmin,
+  newAdminUser,
+  updateAdminUser,
+} from "../controllers/adminUser.js";
 import { requireSuperAdmin } from "../middlewares/access.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-router.route("/register").post(verifyToken, requireSuperAdmin,newAdminUser);
+router.route("/register").post(verifyToken, requireSuperAdmin, newAdminUser);
 router.route("/login").post(adminLogin);
 router.route("/logout").post(adminLogout);
-router.route("/update/:id").patch(verifyToken, requireSuperAdmin,updateAdminUser);
-router.route("/delete/:id").delete(verifyToken, requireSuperAdmin,deleteAdminUser);
+router.route("/update").patch(verifyToken, requireSuperAdmin, updateAdminUser);
+router
+  .route("/delete/:id")
+  .delete(verifyToken, requireSuperAdmin, deleteAdminUser);
+router
+  .route("/getAdmins")
+  .get(verifyToken, requireSuperAdmin, getAllAdminsExceptSuperAdmin);
 
 export default router;
